e2bc064c60bd23d09e046c7d7d61ad629f909904
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-02 15:26:27 -0800

    fixed firebase storage

--- src/Components/ImageUpload/ImageUpload.js
+++ src/Components/ImageUpload/ImageUpload.js
@@ -1,9 +1,11 @@
 import { Input, Button } from '@material-ui/core'
 import React, { useState } from 'react'
 import './ImageUpload.css'
-import { storage, db } from '../../firebase'
+import firebase from 'firebase';
+import { db, storage } from '../../firebase'
 
-function ImageUpload() {
+
+function ImageUpload({ username }) {
     const [image, setImage] = useState(null)
     const [progress, setProgress] = useState(0)
     const [caption, setCaption] = useState('') 
@@ -15,7 +17,34 @@ function ImageUpload() {
     };
     const handleUpload = () => {
         const uploadTask = storage.ref(`images/${image.name}`).put(image);
+        uploadTask.on(
+            "stateChanged",
+            (snapshot) => {
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.TotalBytes) * 100
+                );
+                setProgress(progress);
+            },
+            (error) => {
+                console.log(error);
+                alert(error.message);
+            },
+            () => {
+                storage
+                .ref("images")
+                .child(image.name)
+                .getDownloadURL()
+                .then(url =>{
+                    db.collection("posts").add({
+                        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                        caption: caption,
+                        imageUrl: url,
+                        username: username,
+                    })
+                })
 
+            }
+        )
     }
     
     return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,22 @@
 import firebase from 'firebase';
 
+
   const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDqeFpk23cU_kXNd8yXUuL1JoMRfNDcGGA",
-    authDomain: "legitenough-e1470.firebaseapp.com",
-    projectId: "legitenough-e1470",
-    storageBucket: "legitenough-e1470.appspot.com",
-    messagingSenderId: "525590042415",
-    appId: "1:525590042415:web:a4a6e87d6ed45b030ff3da",
-    measurementId: "G-60G83MR3J0"
+  apiKey: "AIzaSyDqeFpk23cU_kXNd8yXUuL1JoMRfNDcGGA",
+  authDomain: "legitenough-e1470.firebaseapp.com",
+  databaseURL: "https://legitenough-e1470-default-rtdb.firebaseio.com",
+  projectId: "legitenough-e1470",
+  storageBucket: "legitenough-e1470.appspot.com",
+  messagingSenderId: "525590042415",
+  appId: "1:525590042415:web:a4a6e87d6ed45b030ff3da",
+  measurementId: "G-60G83MR3J0"
+
   });
   
   const db = firebaseApp.firestore();
   const auth = firebase.auth();
+  const storage = firebase.storage();
   const provider = new firebase.auth.GoogleAuthProvider();
   
-  export { db, auth, provider };
-  export default db; 
+  
+  export { db, auth, provider, storage };
