7e689c77d6dd62d466fdeae284447dbf57e24f5f
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-01 23:31:28 -0800

    working 2

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Post.css'
+import Avatar  from '@material-ui/core/Avatar';
+
 
 function Post() {
     return (
-        <div className="Post">
+        <div className="post">
+            <div className="post__header">
+            </div>
+            <Avatar className="post__avatar" alt="Juzva8" src="/static/images/avatar/1.jpg"
+            />
             <h3>Username</h3>
+
             {/* header -> avatar + username */}
             <img className="Post__image" src="https://avatars.githubusercontent.com/u/72834046?s=460&u=13200316b9a5b284830062c1c1bf2a347616e043&v=4" alt="post"/>
       
