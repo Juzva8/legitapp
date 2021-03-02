1e8bd4dfbe9f4f3c0ab34e82e23eea9169025639
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-02 14:08:36 -0800

    upload functionality

--- src/Components/ImageUpload/ImageUpload.js
+++ src/Components/ImageUpload/ImageUpload.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import { Input, Button } from '@material-ui/core'
+import React, { useState } from 'react'
 import './ImageUpload.css'
+import { storage, db } from '../../firebase'
 
 function ImageUpload() {
+    const [image, setImage] = useState(null)
+    const [progress, setProgress] = useState(0)
+    const [caption, setCaption] = useState('') 
+
+    const handleChange = (e) => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    };
+    const handleUpload = () => {
+
+
+    }
+    
     return (
         <div>
+        <h1> Hello legit</h1>
         {/* I need ... */} 
         {/* caption input */}
         {/* file picker a */}
         {/*post button*/}
+        <input type="text" placeholder="Enter a caption" onChange={event => setCaption(event.target.value)} value={caption} />
+        <input type="file" onChange={handleChange} />
+        <Button className="imageupload__button" onClick={handleUpload}>
+        upload </Button>
         </div>
     )
 }
