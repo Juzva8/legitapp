88ae5b957322a5ba2491b2bc781e389c92df0614
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-01 23:27:02 -0800

    working

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import './Post.css'
 
 function Post() {
     return (
-        <div className>
+        <div className="Post">
             <h3>Username</h3>
-            {/* header */}
-            {/* image */}
-            {/* user name + caption */}
+            {/* header -> avatar + username */}
+            <img className="Post__image" src="https://avatars.githubusercontent.com/u/72834046?s=460&u=13200316b9a5b284830062c1c1bf2a347616e043&v=4" alt="post"/>
+      
+            <h4 className="Post__text"><strong>Juzva8</strong> coding a lot</h4>
+         
 
         </div>
     )
