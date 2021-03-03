41001d38113fc23ab8dbc9c90c4693ade9aa94a3
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-02 23:12:21 -0800

    made comments work

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react'
 import './Post.css'
 import Avatar  from '@material-ui/core/Avatar';
 import { db } from '../../firebase'
+import firebase from 'firebase';
 
 
 
-function Post({ postId, username, caption, imageUrl }) {
+function Post({ postId, user, username, caption, imageUrl }) {
     const [comments, setComments] = useState ([]);
     const [comment, setComment] = useState ('');
 
@@ -16,6 +17,7 @@ function Post({ postId, username, caption, imageUrl }) {
         .collection("posts")
         .doc(postId)
         .collection("comments")
+        .orderBy('timestamp', 'desc')
         .onSnapshot((snapshot) => {
             setComments(snapshot.docs.map((doc) => doc.data()));
         });
@@ -26,7 +28,13 @@ function Post({ postId, username, caption, imageUrl }) {
 }, [postId]);
 
         const postComment = (event) => {
-
+            event.preventDefault();
+            db.collection("posts").doc(postId).collection("comments").add({
+                text: comment,
+                username: user.displayName,
+                timestamp:firebase.firestore.FieldValue.serverTimestamp()
+            })
+            setComment('');
         }
 
 
